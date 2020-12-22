864c8f384d0ceca25d916265f03c464f20c4e074
panishev95 <andreypanishev@hotmail.com>
2020-12-22 09:02:51 +0300

    Initial Commit

---
+++ Pull to Slide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pull to Slide
+//
+//  Created by Andrei Panishev on 22.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
